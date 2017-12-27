c70d9c5ac36015d02ea863e7da679b327cbcf08a
Dan Ellis <daniel.ellis.research@googlemail.com>
2017-12-27 01:46:18 +0000

    updating branch

--- yarcc_easy/__init__.py
+++ yarcc_easy/__init__.py
@@ -1,4 +1,4 @@
-import glob,sys,os,re
+import sys,os,re
 from argparse import ArgumentParser
 
 
@@ -6,10 +6,10 @@ from argparse import ArgumentParser
 
 A submission program for YARCC
 
-Queue data 
-its-48hour	48 hours	69	1462	16/20/24/28 - 64/128/256/512GB
-64/128its-7day	7 days	10	180	16/20 - 64/128GB
-its-14day	14 days	8	112	16/20 - 64/128GB
+Queue data
+its-48hour	    48 hours	69	1462	16/20/24/28 - 64/128/256/512GB
+64/128its-7day	7 days	    10	180	16/20 - 64/128GB
+its-14day	    14 days	     8	112	16/20 - 64/128GB
 its-nvidia-k20	48 hours & 7 days	2	34	10/24 - 64GB
 its-nvidia-k40	48 hours & 7 days	2	20	10 - 64GB
 its-nvidia-k80	48 hours	1	10	10 - 128GB
@@ -78,58 +78,58 @@ args = parser.parse_args()
 
 #args.perplexity
 
-if args.command :    
+if args.command :
 
-    
+
     print u'\u001b[34m Preparing submission script \u001b[0m'
 
     string = '#!/usr/bin/bash\n'
-    
+
     string += '### Genearated using https://github.com/wolfiex/YARCC_easy_submit.git ####\n'
-    
+
     string +=  '#$ %s\n'%args.job_variables
-    
+
     string +=  '#$ -cwd -V \n'
-    
+
     string +=  '#$ -l h_rt=%s\n'%args.time
-    
-    
+
+
     if args.ncores>1:
-    
+
         if args.distributed:
             string +=  '#$ -pe ib %d\n'%args.ncores
         else:
             string +=  '#$ -pe smp %d\n'%args.ncores
-            
-            
+
+
     if float(args.mem)/args.ncores < args.ncores:
         string +=  '#$ -l h_vmem=%sG\n'%1
     else:
         string +=  '#$ -l h_vmem=%sG\n'%float(args.mem)/args.ncores
-    
+
     if args.email:
         string +=  '#$ -m be\n'
         string +=  '#$ -M %s"york.ac.uk\n'%usr
-        
+
     #string +=  '#$ %s\n'%args.job_variables
-    
+
     if not os.path.dirname(args.output_dir):
         os.makedirs(args.output_dir)
     string +=  '#$ -o %s\n'%args.output_dir
-    
+
     if not os.path.exists(args.error_dir):
         os.makedirs(args.error_dir)
     string +=  '#$ -e %s\n'%args.error_dir
-    
+
     if args.source != '':
         string +=  'source %s\n'%args.source
-        
-    string += '%s \n'%args.command 
-    
+
+    string += '%s \n'%args.command
+
     with open('./yarcc.job','w') as f:
         f.write(string)
-    
-    
+
+
 
 if args.run:
     print u'\u001b[34m Submiting Job \u001b[0m'
@@ -138,63 +138,62 @@ if args.run:
         runfile = args.runfile
     else:
         runfile = 'yarcc.job'
-        
+
     if not os.path.exists(runfile):
         print u'\u001b[31m No file found to run: \u001b[0m ' + runfile
-    else: 
+    else:
         newfile = os.popen('qsub %s'%runfile).read()
         newid = newfile.split(' ')[2]
         validity = os.popen('qalter -w p %s'%newid).read()
-        print validity
+        print '\n'.join(filter(lambda x:'has no permission' not in x, validity.split('\n')))
         if 'no suitable queues' in validity:
-              print u'\u001b[34m\nNo suitable queues: Terminating %s\n\u001b[0m'%newid
+              print u'\u001b[34m\nNo queues: Terminating %s\n\u001b[0m'%newid
               os.system('qdel %s'%newid)
-        
-        
-    
-    
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+
 if args.watch:
     os.system('watch qstat -u %s'%usr)
-    
-    
+
+
 if args.checkall:
     runs = os.popen('qstat -u %s'%usr).read()
-    runs = re.findall(r'(\b\d+[\w\. A-z/]+[\d:]+\b)',runs) 
+    runs = re.findall(r'(\b\d+[\w\. A-z/]+[\d:]+\b)',runs)
     print u'\u001b[34m\n Checking all runs:\n\u001b[0m'
-    
+
     for k in runs:
         k = k.split(' ')
         print ' '.join(k[:4])+' - '+ str(os.popen('qalter -w p %s'%k[0]).read())
-    
-    
+
+
 if args.delete:
     runs = os.popen('qstat -u %s'%usr).read()
     #runlist = re.findall(r'(\b\d+[\w\s\. \W]+[\t\s ]+\d+/\d+/\d+[\t\s ]\d+:\d+:\d+)',runs)
     runs = re.findall(r'(\b\d+[\w\. A-z/]+[\d:]+\b)',runs)
     print u'\u001b[34m\n Select runs to delete (space separated):\n\u001b[0m'
     if len(runs)>1:
-    
-        print '   a - Delete all' 
-        
+
+        print '   a - Delete all'
+
         for i,j in enumerate(runs):
             print '%4d - %s'%(i,j)
-    
+
         s = raw_input(u'\u001b[35mMake selection here: \n\u001b[0m')
-        
+
         for k in s.split(' '):
             if k=='': continue
             if k == 'a': os.system('qdel -u %s'%usr)
-            else: 
+            else:
                 k = runs[int(k.replace(' ',''))].split(' ')[0]
-                  
-            os.system('qdel %s'%k)
-            
-            
-            
-sys.exit('Finished fine')
 
+            os.system('qdel %s'%k)
+
+
+
+sys.exit('Finished fine')
